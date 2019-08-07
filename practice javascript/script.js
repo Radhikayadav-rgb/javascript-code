e9d600a4ca26c7a8942af25d83d81478c8d4aa46
Radhikayadav-rgb <reddyradhika.r@gmail.com>
2019-08-07 04:19:23 -0700

    first commit

---
+++ practice javascript/script.js
@@ -0,0 +1,3 @@
+alert("Hello i am javacript alert");
+document.write('<h1> Hello from javascript"s write </h1>');
+alert("Thanks for visiting and keep coming back");
---
+++ practice javascript/script1.js
@@ -0,0 +1,12 @@
+<!-- working with number-->
+
+var noOfSecsinaMinute=60;
+var noOfMinsinaHour=60;
+var noOfHoursinDays=24;
+var noOfDaysinWeek=7;
+var noOfDaysInYear=365;
+var noOfDaysInMonth=31;
+var noOfweeksInyear=52;
+var secondsPerDay = noOfSecsinaMinute * noOfMinsinaHour * noOfHoursinDays;
+document.write('There are '+secondsPerDay+' seconds in a day');
+	
